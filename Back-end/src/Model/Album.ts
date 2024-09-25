7c642ea7b244b8f7b24d695765b26298f3603be8
takeleberhe <77755440+takeleberhe@users.noreply.github.com>
2024-09-25 06:07:47 +0300

    refactor

--- Back-end/src/Model/Album.ts
+++ Back-end/src/Model/Album.ts
@@ -1,24 +1,3 @@
-/* import mongoose, { Schema, Document } from "mongoose";
-interface IAlbum extends Document {
-  title: string;
-  artist: string;
-  songCount: number;
-  songs: mongoose.Types.ObjectId[];
-  createdAt: Date;
-  updatedAt: Date;
-}
-const AlbumSchema: Schema = new Schema(
-  {
-    title: { type: String, required: true },
-    artist: { type: String, required: true },
-    songCount: { type: Number },
-    songs: [{ type: mongoose.Types.ObjectId, ref: "Song" }],
-  },
-  { timestamps: true }
-);
-const Album = mongoose.model<IAlbum>("Album", AlbumSchema);
-export default Album;
- */
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IAlbum extends Document {
